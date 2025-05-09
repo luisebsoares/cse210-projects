f4f276aef9d32fbec35c0cde43c5e09a49dc6275
LuisBrasil <lsoares@byupathway.edu>
2025-05-08 20:46:16 -0600

    update

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1, 101);
+        int guess = -1;
+
+        while (guess != magic_number)
+        {
+            Console.WriteLine("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magic_number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magic_number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it right!");
+            }
+        }
     }
 }
