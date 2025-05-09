31492b772400829883228dffef578ec96edea87d
LuisBrasil <lsoares@byupathway.edu>
2025-05-08 20:29:55 -0600

    update

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -10,35 +10,35 @@ class Program
 
         string letter = "";
 
-        if (percentage >= 90) ;
+        if (percentage >= 90)
         {
             letter = "A";
         }
 
-        else if (percentage >= 80) ;
+        else if (percentage >= 80)
         {
             letter = "B";
         }
 
-        else if (percentage >= 70) ;
+        else if (percentage >= 70)
         {
             letter = "C";
         }
 
-        else if (percentage >= 60) ;
+        else if (percentage >= 60)
         {
             letter = "D";
         }
 
-        else if (percentage <= 60) ;
+        else if (percentage <= 60)
         {
             letter = "F";
         }
 
         Console.WriteLine($"Your grade is: {letter}");
-        if (percentage >= 70);
+        if (percentage >= 70)
         {
-            Console.WriteLine("You passed!");
+            Console.WriteLine("Congratulations! You passed!");
         }
         else
         {
