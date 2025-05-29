9635b6304473246709f2f0291ba6ade1d9f7d5a1
LuisBrasil <lsoares@byupathway.edu>
2025-05-29 17:55:34 -0600

    homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("John Smith", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Jane Doe", "Fractions", "Section 1.1", "Problems 1-10");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Emily Johnson", "History", "The American Revolution");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInfo());
     }
 }
