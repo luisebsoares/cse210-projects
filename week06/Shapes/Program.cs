2a4ba1424566a3943033fcaba69980c6b4fb30e4
LuisBrasil <lsoares@byupathway.edu>
2025-06-09 19:40:35 -0600

    homework

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Shape square = new Square("Red", 4.0);
+        Shape rectangle = new Rectangle("Blue", 5.0, 3.0);
+        Shape circle = new Circle("Green", 2.5);
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea():F2}");
+        }
     }
 }
